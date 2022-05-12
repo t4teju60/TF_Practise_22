99b2de79ed66dcd4b7277b710f6ecf4ca0abb6e3
Tejesh <t4teju60@gmail.com>
2022-05-12 15:30:05 +0530

    Variabilizing the values-removing hard coded values

--- vpc/main.tf
+++ vpc/main.tf
@@ -14,37 +14,47 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-resource "aws_subnet" "pub_subnet_1" {
+# resource "aws_subnet" "pub_subnet_1" {
+#     vpc_id = aws_vpc.my_vpc.id
+#     cidr_block = var.cidr_ranges[0]
+#     availability_zone = "us-east-1a"
+#     tags = {
+#         Name = "pub_subnet_1"
+#     }
+# }
+
+resource "aws_subnet" "subnets" {
     vpc_id = aws_vpc.my_vpc.id
-    cidr_block = var.sub1_cidr
-    availability_zone = "us-east-1a"
+    count = 6
+    cidr_block = var.cidr_ranges[count.index]
+    availability_zone = var.availaibility_zones[count.index]
     tags = {
-        Name = "pub_subnet_1"
+        Name = "web-${count.index}"
+        Name_alternate = var.subnets[count.index]
     }
 }
 
-resource "aws_subnet" "pub_subnet_2" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = var.sub2_cidr
-    availability_zone = "us-east-1b"
-    tags = {
-        Name = "pub_subnet_2"
-    }
+variable "subnets" {
+    type = list(string)
+    default = ["App1", "App2", "App3", "App4","App5","App6"]
+    description = "Name of subnets"
+}
+
+variable "cidr_ranges" {
+    type = list(string)
+    default = ["192.168.1.0/24","192.168.2.0/24","192.168.3.0/24","192.168.4.0/24","192.168.5.0/24","192.168.6.0/24"]
+    description = "cidr ranges for all subnets"
+}
+
+variable "availaibility_zones" {
+    type = list(string)
+    default = ["us-east-1a","us-east-1b","us-east-1c","us-east-1d","us-east-1e","us-east-1f"]
 }
 
 variable "region" {
     default = "us-east-1"
-    type = string
 }
 
 variable "vpc_cidr" {
     default = "192.168.0.0/16"
-}
-
-variable "sub1_cidr" {
-  default = "192.168.1.0/24"
-}
-
-variable "sub2_cidr" {
-    default = "192.168.2.0/24"
 }
