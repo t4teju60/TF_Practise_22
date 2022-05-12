6965418422d5d6ca1e32ba0db91356e1652d16b2
Tejesh <t4teju60@gmail.com>
2022-05-12 11:39:36 +0530

    provider aws aunthentication using shared config file instead of env vars

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
     region = "us-east-1"
+    shared_credentials_files = ["/Users/tejesh.karri/.aws/credentails"]
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -8,5 +9,15 @@ resource "aws_vpc" "my_vpc" {
   enable_dns_hostnames = true
   tags = {
       Name = "from_tf"
+      env = "dev"
   }
+}
+
+resource "aws_subnet" "pub_subnet_1" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "192.168.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "pub_subnet_1"
+    }
 }
