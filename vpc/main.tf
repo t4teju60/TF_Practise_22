ffce0e199a05bc423815a394e7a84345c1ad21ae
Tejesh <t4teju60@gmail.com>
2022-05-12 14:33:18 +0530

    Adding basic variables

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+    # region = "us-east-1"
+    region = var.region
     shared_credentials_files = ["/Users/tejesh.karri/.aws/credentails"]
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "192.168.0.0/16"
+  cidr_block = var.vpc_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
@@ -15,7 +16,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "pub_subnet_1" {
     vpc_id = aws_vpc.my_vpc.id
-    cidr_block = "192.168.1.0/24"
+    cidr_block = var.sub1_cidr
     availability_zone = "us-east-1a"
     tags = {
         Name = "pub_subnet_1"
@@ -24,9 +25,26 @@ resource "aws_subnet" "pub_subnet_1" {
 
 resource "aws_subnet" "pub_subnet_2" {
     vpc_id = aws_vpc.my_vpc.id
-    cidr_block = "192.168.2.0/24"
+    cidr_block = var.sub2_cidr
     availability_zone = "us-east-1b"
     tags = {
         Name = "pub_subnet_2"
     }
+}
+
+variable "region" {
+    default = "us-east-1"
+    type = string
+}
+
+variable "vpc_cidr" {
+    default = "192.168.0.0/16"
+}
+
+variable "sub1_cidr" {
+  default = "192.168.1.0/24"
+}
+
+variable "sub2_cidr" {
+    default = "192.168.2.0/24"
 }
